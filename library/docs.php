e8bfb3071145ce5bcf2c350fcf34eb47af25301d
joshre <joshre@me.com>
2018-03-08 14:13:30 -0800

    Docs

---
+++ library/docs.php
@@ -0,0 +1,18 @@
+<style type="text/css" media="screen">li{ padding-left: 20px;}.docitem{margin-bottom: 10px;}</style>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#80-watt-district-pbid-wordpress-documentation">80 Watt District PBID Wordpress Documentation</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#wordpress-overview">WordPress Overview</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#logging-in">Logging In</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#media-library">Media Library</a>
+<ul>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-a-media-item">Adding a Media Item</a></li>
+</ul></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-editing-a-page">Adding/Editing a Page</a>
+<ul>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-a-new-page">Adding A New Page</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#content">Content</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#published-draft-schedulded-private">Published / Draft / Scheduled / Private</a></li>
+	<li><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#editing-a-page">Editing a Page</a></li>
+</ul></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#adding-featured-images">Adding Featured Images</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#updating-site-options">Updating Site Options</a></div>
+<div class="docitem"><a href="<?php echo get_template_directory_uri(); ?>/library/docs/#updating-the-footer">Updating the Footer</a></div>
